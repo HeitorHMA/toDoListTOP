59b44491f4c128e6c0c89a255856adaee3729b14
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-03-03 17:27:18 -0300

    N

--- src/displayProjects.js
+++ src/displayProjects.js
@@ -5,9 +5,9 @@ function displayProjects(){
     const pageContent = document.querySelector("#projectDiv")
     for(let i = 0; i < userProjects.length; i++){
         
-        const projectDisplayDiv= document.createElement("div");
+        const projectDisplayDiv= document.createElement("h1");
         const projectName = userProjects[i].name;
-        projectName.innerHTML = `<h1>${projectName}</h1>`;
+        projectName.textContent = "projectName";
         pageContent.appendChild(projectDisplayDiv);
       }
     };
