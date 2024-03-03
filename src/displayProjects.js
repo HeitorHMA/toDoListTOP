f36882acb8c838ca9b68c27f46f85f4363e2422d
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-03-03 17:22:39 -0300

    Add innerHtml

--- src/displayProjects.js
+++ src/displayProjects.js
@@ -1,12 +1,14 @@
 import {userProjects} from "./userProjects";
 
+
 function displayProjects(){
     const pageContent = document.querySelector("#projectDiv")
     for(let i = 0; i < userProjects.length; i++){
         
-        const projectName = document.createElement("h1");
-        projectName.textContent = `${userProjects[i].name}`
-        pageContent.appendChild(projectName);
+        const projectDisplayDiv= document.createElement("div");
+        const projectName = userProjects[i].name;
+        projectName.innerHTML = `<h1>${projectName}</h1>`;
+        pageContent.appendChild(projectDisplayDiv);
       }
     };
-export default displayProjects
+export default displayProjects 
