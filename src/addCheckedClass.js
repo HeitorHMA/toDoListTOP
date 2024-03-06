f304cb915cd28bbab191018955050ed30e918f00
Heitor Henrique <156817493+HeitorHMA@users.noreply.github.com>
2024-03-06 19:24:20 -0300

    Try for loop

--- src/addCheckedClass.js
+++ src/addCheckedClass.js
@@ -3,14 +3,14 @@ import { userProjects } from "./userProjects";
 function addCheckedClass(){
     const projectDiv = document.querySelector(".project-div");
     const checkBox = document.querySelector(".chckBox");
-for (let i ; i < userProjects.length ; i++){
+for(let i ; i > userProjects.length; i++){
     checkBox.addEventListener("click", addCheck);
-}
+};
 function addCheck(){
    if( projectDiv.classList.contains("checked")){
     projectDiv.classList.remove("checked")}
     else{
         projectDiv.classList.add("checked")
-    }
+    };
 }}
 export default addCheckedClass
